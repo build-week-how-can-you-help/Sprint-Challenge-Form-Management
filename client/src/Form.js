2dc4dceab8644ed5aed4c250932d7ca2850bd794
Scott Renz <scottrenz@scottrenz.com>
2019-08-09 13:50:34 -0400

    putting out data

---
+++ client/src/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormikLoginForm from './FormikLoginForm';
+
+class FormMy extends React.Component {
+    constructor() { 
+      super();
+      this.state = {};
+    }
+    render() {
+      return (
+    <div >
+      <FormikLoginForm  />
+  </div>
+)
+    }
+  } 
+
+  export default FormMy;
